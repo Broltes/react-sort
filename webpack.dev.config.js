c161f47101d239b1440f7be5c6047082c3cad7cc
Broltes <broltes@qq.com>
2016-10-17 11:08:57 +0800

    no message

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -31,6 +31,7 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loaders: [
+                    'react-hot',
                     'babel'
                 ]
             }, {
